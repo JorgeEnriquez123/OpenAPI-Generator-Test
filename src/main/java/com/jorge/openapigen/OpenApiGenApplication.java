c1c1de79a897edd07be8eefd5a011975367a0253
Jorge <joseph_adagio_7700@hotmail.com>
2025-03-07 00:55:08 -0500

    project start

---
+++ src/main/java/com/jorge/openapigen/OpenApiGenApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.openapigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorge/openapigen/OpenApiGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.openapigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
